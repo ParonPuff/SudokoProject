a26f745f56f35286dd2e0d6d93dcc1d586067b98
Simon <simon@karlsen.se>
2023-07-05 23:40:49 +0200

    starting on board and cell implementation

---
+++ board.c
@@ -0,0 +1,40 @@
+#include "board.h"
+
+struct board{
+
+};
+struct cell{
+
+};
+
+Board *make_board_from_file(FILE *board){
+
+}
+
+Board *set_cell(Board *board, Cell *sudokuCell, int value){
+
+}
+
+bool board_is_valid(Board *board){
+
+}
+
+bool board_is_filled(Board *board){
+
+}
+
+Cell *get_cell_min_possibilities(Board *board){
+
+}
+
+int get_possibility(Cell *cell){
+
+}
+
+void print_board(Board *board){
+
+}
+
+void destroy_board(Board *board){
+
+}
